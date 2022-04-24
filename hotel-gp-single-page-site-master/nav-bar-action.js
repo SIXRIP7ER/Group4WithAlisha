c2cee6ad84a99021724c833d3e4fd8f3dee2c7aa
asur3329 <92345663+asur3329@users.noreply.github.com>
2022-04-24 13:39:01 +1000

    Update and rename script.js to nav-bar-action.js

--- hotel-gp-single-page-site-master/nav-bar-action.js
+++ hotel-gp-single-page-site-master/nav-bar-action.js
@@ -3,16 +3,19 @@ const cancelBtn = document.getElementById('cancel-btn');
 const sideNav = document.getElementById('sidenav');
 const modal = document.getElementById('modal');
 
+<!-- Adds event listener to when the nav bar is pressed -->
 navBtn.addEventListener("click", function(){
     sideNav.classList.add('show');
     modal.classList.add('showModal');
 });
 
+<!-- Adds event listener to when the cancel button is pressed -->
 cancelBtn.addEventListener('click', function(){
     sideNav.classList.remove('show');
     modal.classList.remove('showModal');
 });
 
+<!-- Adds event listener to view the nav bar and all its tabs -->
 window.addEventListener('click', function(event){
     if(event.target === modal){
         sideNav.classList.remove('show');
